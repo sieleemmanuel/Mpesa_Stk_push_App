acd6b8f4814f82366552a882a9c84d4224c70552
kimsiele <sielekim.emmanuel@gmail.com>
2022-05-24 15:22:54 +0300

    fixes added

--- app/src/main/java/com/siele/mpesastk/utils/Constants.kt
+++ app/src/main/java/com/siele/mpesastk/utils/Constants.kt
@@ -1,13 +1,11 @@
 package com.siele.mpesastk.utils
 
 import com.androidstudy.daraja.util.TransactionType
+import com.siele.mpesastk.BuildConfig
 
 object Constants {
-    var CALLBACK_URL ="https://mydomain.com/path"
-    // "https://mpesa-requestbin.herokuapp.com/1ddvcf71"
+    var CALLBACK_URL ="https://mpesa-requestbin.herokuapp.com/1h2mk4d1"
     var BUSINESS_SHORTCODE = "174379"
     var ACCOUNT_TYPE = TransactionType.CustomerPayBillOnline
-    var PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-    //var PASSKEY= "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIwNTEzMTMxMDU5"
-    //var PASSKEY = "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIwNTEzMTUwODI5"
-}
+    var PASSKEY = BuildConfig.PASSKEY
+    }
